4f99f5fd7ffaa457fdf60bdae660ab80cc190bcd
Gag98 <gaghovsepyan9119@gmail.com>
2024-09-15 19:45:21 +0300

    Changed comment language.

--- todo/database/base.py
+++ todo/database/base.py
@@ -3,31 +3,32 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from todo.config import settings
 
-# Определение базового каталога и создание пути к базе данных
+# Define the base directory and create the database path
 BASE_DIR = os.path.dirname(os.path.abspath(__name__))
 db_path = os.path.join(BASE_DIR, 'todo', 'database', 'DB')
 if not os.path.exists(db_path):
     os.makedirs(db_path)
 
-# Создание базового класса для всех моделей
+# Create a base class for all models
 Base = declarative_base()
 
-# Создание движка базы данных
+# Creating a database engine
 engine = create_engine(
     settings.db_url,
     connect_args={'check_same_thread': False},
     echo=True
 )
 
-# Создание фабрики сессий для взаимодействия с базой данных
+# Create a session factory to interact with the database
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
+
 def get_db():
-    """Создает и предоставляет сессию базы данных.
+    """Creates and provides a database session.
 
     Возвращает:
         generator: Сессия базы данных. Сессия закрывается после использования.
@@ -37,3 +38,4 @@ def get_db():
         yield db_session_local
     finally:
         db_session_local.close()
+
